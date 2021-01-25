867ad483da513eeab25e4d60146c653227a7c55e
prem <premplayz@gmail.com>
2021-01-25 10:18:19 +0700

    Add project files.

---
+++ Assignment 6 Coffee Shop/Drink.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_6_Coffee_Shop
+{
+    class Drink
+    {
+        private string Name;
+        private int Price;
+        private int TimesSold = 0;
+
+        public static int ItemCount = 0;
+        public static string[] Menu = new string[3];
+
+        public Drink(string Name, int Price)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            TimesSold = TimesSold + 1;
+            bool New = true;
+            for(int i = 0; i < Menu.Length; i += 1)
+            {
+                if (Name == Menu[i])
+                {
+                    New = false;
+                }
+            }
+            if (New == true)
+            {
+                Menu[ItemCount] = Name;
+                ItemCount += 1;
+            }
+        }
+        public static void GetItems()
+        {
+            for (int i = 0; i < Drink.Menu.Length; i += 1)
+            {
+                Console.WriteLine(Menu[i]);
+            }
+        }
+        public int AmountSold()
+        {
+            return TimesSold;
+        }
+    }
+}
---
+++ Assignment 6 Coffee Shop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Assignment_6_Coffee_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drink Coffee = new Drink("Coffee", 50);
+            Drink Tea = new Drink("Tea", 45);
+            Drink.GetItems();
+            Console.WriteLine(Coffee.AmountSold());
+        }
+    }
+}
