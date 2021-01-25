8bb42b88d6d78d54ed65b6b81474712b210772f5
prem <premplayz@gmail.com>
2021-01-25 11:13:38 +0700

    New stuff

--- Assignment 6 Coffee Shop/Drink.cs
+++ Assignment 6 Coffee Shop/Drink.cs
@@ -8,40 +8,29 @@ namespace Assignment_6_Coffee_Shop
     {
         private string Name;
         private int Price;
-        private int TimesSold = 0;
-
-        public static int ItemCount = 0;
-        public static string[] Menu = new string[3];
-
-        public Drink(string Name, int Price)
+        public Drink(string Name)
         {
             this.Name = Name;
-            this.Price = Price;
-            TimesSold = TimesSold + 1;
-            bool New = true;
-            for(int i = 0; i < Menu.Length; i += 1)
+            if (Name == "Coffee")
             {
-                if (Name == Menu[i])
-                {
-                    New = false;
-                }
+                Price = 50;
             }
-            if (New == true)
+            else if (Name == "Tea")
             {
-                Menu[ItemCount] = Name;
-                ItemCount += 1;
+                Price = 45;
+            }
+            else if (Name == "Juice")
+            {
+                Price = 30;
             }
         }
-        public static void GetItems()
+        public string GetName()
         {
-            for (int i = 0; i < Drink.Menu.Length; i += 1)
-            {
-                Console.WriteLine(Menu[i]);
-            }
+            return Name;
         }
-        public int AmountSold()
+        public int GetPrice()
         {
-            return TimesSold;
+            return Price;
         }
     }
 }
--- Assignment 6 Coffee Shop/Program.cs
+++ Assignment 6 Coffee Shop/Program.cs
@@ -6,10 +6,58 @@ namespace Assignment_6_Coffee_Shop
     {
         static void Main(string[] args)
         {
-            Drink Coffee = new Drink("Coffee", 50);
-            Drink Tea = new Drink("Tea", 45);
-            Drink.GetItems();
-            Console.WriteLine(Coffee.AmountSold());
+            Drink[] Drinks = new Drink[10];
+
+            Drinks[0] = new Drink("Coffee");
+            Drinks[1] = new Drink("Tea");
+            Drinks[2] = new Drink("Juice");
+            Drinks[3] = new Drink("Coffee");
+
+            int Stock = 0;
+            int Total = 0;
+
+            for (int i = 0; i < Drinks.Length; i += 1)
+            {
+                if (Drinks[i] != null)
+                {
+                    Console.WriteLine(Drinks[i].GetName() + " " + Drinks[i].GetPrice());
+                    Total += Drinks[i].GetPrice();
+                }
+                else
+                {
+                    //Console.WriteLine(i + " Is Null");
+                    Stock += 1;
+                }
+            }
+            Console.WriteLine("Stock Remainding: " + Stock);
+            Console.WriteLine(Total + " Baht");
+
+
+            int CountOfCoffee = 0;
+            int CountOfTea = 0;
+            int CountOfJuice = 0;
+
+            for (int i = 0; i < Drinks.Length; i += 1)
+            {
+                if (Drinks[i] != null)
+                {
+                    if (Drinks[i].GetName() == "Coffee")
+                    {
+                        CountOfCoffee += 1;
+                    }
+                    else if (Drinks[i].GetName() == "Tea")
+                    {
+                        CountOfTea += 1;
+                    }
+                    else if (Drinks[i].GetName() == "Juice")
+                    {
+                        CountOfJuice += 1;
+                    }
+                }
+            }
+            Console.WriteLine("Coffee: " + CountOfCoffee);
+            Console.WriteLine("Tea: " + CountOfTea);
+            Console.WriteLine("Juice: " + CountOfJuice);
         }
     }
 }
